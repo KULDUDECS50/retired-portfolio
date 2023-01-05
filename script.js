d735b3846142aa21cf1625870c47f15764a2c7b9
Kuldeep <kuldeep.debnath@gmail.com>
2023-01-05 17:37:50 -0600

    got carsoel

--- script.js
+++ script.js
@@ -47,21 +47,21 @@ darkModeToggle.addEventListener('click', () => {
   }
 });
 //card swiper
-var carouselWidth = $(".carousel-inner")[0].scrollWidth;
-var cardWidth = $(".carousel-item").width();
-var scrollPosition = 0;
-$(".carousel-control-next").on("click", function () {
-  if (scrollPosition < (carouselWidth - cardWidth * 4)) { //check if you can go any further
-    scrollPosition += cardWidth;  //update scroll position
-    $(".carousel-inner").animate({ scrollLeft: scrollPosition },600); //scroll left
-  }
-});
-$(".carousel-control-prev").on("click", function () {
-  if (scrollPosition > 0) {
-    scrollPosition -= cardWidth;
-    $(".carousel-inner").animate(
-      { scrollLeft: scrollPosition },
-      600
-    );
-  }
-});
+// var carouselWidth = $(".carousel-inner")[0].scrollWidth;
+// var cardWidth = $(".carousel-item").width();
+// var scrollPosition = 0;
+// $(".carousel-control-next").on("click", function () {
+//   if (scrollPosition < (carouselWidth - cardWidth * 4)) { //check if you can go any further
+//     scrollPosition += cardWidth;  //update scroll position
+//     $(".carousel-inner").animate({ scrollLeft: scrollPosition },600); //scroll left
+//   }
+// });
+// $(".carousel-control-prev").on("click", function () {
+//   if (scrollPosition > 0) {
+//     scrollPosition -= cardWidth;
+//     $(".carousel-inner").animate(
+//       { scrollLeft: scrollPosition },
+//       600
+//     );
+//   }
+// });
