ffd2f92877904338de7904e05544aab227311af6
Kuldeep <kuldeep.debnath@gmail.com>
2023-01-09 14:52:22 -0600

    lots

--- script.js
+++ script.js
@@ -20,6 +20,8 @@ const enableDarkMode = () => {
 const disableDarkMode = () => {
   // 1. Remove the class from the body
   document.body.classList.remove('darkmode');
+  document.getElementById("projects").body.classList.remove('darkmode');
+
 
   // 2. Update darkMode in localStorage 
   localStorage.setItem('darkMode', null);
